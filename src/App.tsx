9b53d064dd0c6a699a5fff246e9af5fbd7df24be
meh <meh@meh>
2022-10-05 12:58:47 +0700

    substitute with the call

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,14 @@ import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+/**
+ * makes the (set of) necessary init-calls for every Ionic Apps.
+ * 1) Core CSS required for Ionic components to work properly
+ * 3) Basic CSS for apps built with Ionic
+ * 5) Optional CSS utils that can be commented out
+ * 
+ */
+import './ionicCoreCss' ;
 
 /* Theme variables */
 import './theme/variables.css';

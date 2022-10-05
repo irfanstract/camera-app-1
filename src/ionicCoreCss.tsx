9acdaa57c7f8c138f0de76bc609944ef8ea8565e
meh <meh@meh>
2022-10-05 13:00:40 +0700

    add this import

--- src/ionicCoreCss.tsx
+++ src/ionicCoreCss.tsx
@@ -22,6 +22,9 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+/* Theme variables */
+import './theme/variables.css';
+
 // TODO
 if ((
    0 

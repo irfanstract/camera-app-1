a8edda00c39299f45fa42a896040c6daa498de08
meh <meh@meh>
2022-10-05 08:24:14 +0700

    change the title

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -15,7 +15,7 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>The Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>

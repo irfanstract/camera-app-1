f21da5fcae24fb4a1212907c3cd78e332d509c48
meh <meh@meh>
2022-10-07 00:35:56 +0700

    add

--- src/components/k-audio/ctx.tsx
+++ src/components/k-audio/ctx.tsx
@@ -80,3 +80,26 @@ class PdMode {
 
 }
 namespace PdMode { ; }  // TS-1205, TS-2702, 
+class TAndTScale {
+
+   withDurativeFactor(f: number ) {
+      return (
+         new TAndTScale(this.t, f * this.tScale, )
+      ) ;
+   }
+   withSpeedFactor(f: number ) {
+      return (
+         new TAndTScale(this.t, this.tScale / f , )
+      ) ;
+   }
+
+   constructor(
+      public t: number ,
+      public tScale: number ,
+   ) {}
+   static initially = () => (
+      new TAndTScale(0, 1, )
+   ) ;
+
+}
+namespace TAndTScale { ; }  // TS-1205, TS-2702, 

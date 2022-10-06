1383e987fb8e4c840ea9ba57942f893d9a036420
meh <meh@meh>
2022-10-06 13:33:31 +0700

    add the two method(s)

--- src/components/AsyncData.tsx
+++ src/components/AsyncData.tsx
@@ -57,6 +57,58 @@ const useUpvBasic = (
       } ;
    }
 ) ;
+const usePromiseValue1 = (
+   function <A >(...[{ src , clearOnSwitch, initialValue, },] : [
+      { src: Promise<MonomorphicDigestFnc<A> > ; } & UpvInitialValueConfig<A> ,
+   ] ) {
+      const {
+         presentValue ,
+         setPresentValueImmediately ,
+         resetImmediately ,
+      } = useUpvBasic({ initialValue, }, )  ;
+      // const [ , pushP, ] = (
+      //    useState<Promise<void> >(async () => {} )
+      // ) ;
+      useLayoutEffect(() => {
+         let valid : boolean = true ;
+         (async () => {
+            if (!valid ) return ;
+            clearOnSwitch && (
+               resetImmediately()
+            )  ;
+            if (!valid ) return ;
+            const doDigest = await src ;
+            if (!valid ) return ;
+            setPresentValueImmediately(doDigest );
+            if (!valid ) {
+               throw Error(`assertion error`) ;
+            }
+         })() ;
+         return (): void => {
+            valid = false ;
+         } ;
+      } , [clearOnSwitch, src, ], );
+      // TODO
+      return (
+         presentValue
+      ) ;
+   }
+) ;
+const usePromiseValue = (
+   function <A >(...[{ src: src0 , ...otherProps },] : [
+      { src: Promise<A> ; } & UpvInitialValueConfig<A> ,
+   ] ) {
+      const src1 = (
+         useMemo(async (): Promise<MonomorphicDigestFnc<A> > => {
+            const newVal = await src0 ;
+            return () => newVal ;
+         }, [ src0, ], )
+      ) ;
+      return (
+         usePromiseValue1({ src: src1, ...otherProps })
+      ) ;
+   }
+) ;
 interface UpvInitialValueConfig<A> {
    /**    
     * if this were `true`, then
@@ -78,4 +130,6 @@ interface UpvInitialValueConfig<A> {
 
 
 export {
+   usePromiseValue ,
+   usePromiseValue1 ,
 } ;

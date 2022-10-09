9630aa6ea4a9a1489673c10a93ebec5ecb58ef02
meh <meh@meh>
2022-10-10 01:29:58 +0700

    add

--- src/components/k-audio/components/ampcomp.tsx
+++ src/components/k-audio/components/ampcomp.tsx
@@ -100,3 +100,145 @@ function useAmpNode(aCtx : BaseAudioContext ) {
 } ;
 
 
+const CConstantValueSrcImpl : (
+   React.FC<(
+      { value : number ; }
+      &
+      { c: AudioNode | AudioParam ; aCtx : BaseAudioContext ; }
+   )>
+) = (
+   ({ c: dest, aCtx, value, }) => {
+      const g1 = (
+         useConstantSrcNde(aCtx, )
+      ) ;
+      useConnectDisconnect(g1, dest, ) ;
+      React["useInsertionEffect"](() => {
+         (
+            g1.offset
+            .setTargetAtTime(value, 0, 0.125, )
+         );
+      } , [value, ] , );
+      // TODO
+      return (
+         <div>
+            <p>
+               constant-source-node : {}
+               <code>{ value }</code> {}
+            </p>
+         </div>
+      ) ;
+   }
+) ;
+const CAmpCompImpl : (
+   React.FC<(
+      { children: React.ReactNode ; }
+      &
+      { value : number | React.ReactElement ; }
+      &
+      { c: AudioNode | AudioParam ; aCtx : BaseAudioContext ; }
+   )>
+) = (
+   ({ value: valueArgument0 , c: dest, aCtx, children: payload = null , }) => {
+      const {
+         valueArgument1 ,
+         vDisplay ,
+      } = (
+         numericOrRElement((
+            valueArgument0
+         ), )
+      ) ;
+      const g1 = (
+         useAmpNode(aCtx, )
+      ) ;
+      useConnectDisconnect(g1, dest, ) ;
+      return (  
+         <div>
+         <p>
+            Amp By {}
+            { vDisplay } {}
+         </p>
+         <ol style={{ display: `flex`, flexDirection: `column-reverse`, }}>
+            <li>
+            <WithGivenDestNd value={g1 } >
+               { payload }
+            </WithGivenDestNd>
+            </li>
+            <li>
+            <WithGivenDestNd value={g1.gain } >
+               { valueArgument1 }
+            </WithGivenDestNd>
+            </li>
+         </ol>
+         </div>
+      ) ;
+   }
+) ;
+const wrp1 = (
+   function <P extends { } >(...[displayName, F10, ] : [
+      displayName: string,
+      impl: (
+         React.FC<(
+            { [k in keyof P ] : P[k] ; } 
+            & 
+            { c : AudioNode | AudioParam ; aCtx : BaseAudioContext ; }
+         ) >
+      ) ,
+   ] ) {
+      const F1 : (
+         React.FC<(
+            { [k in keyof P ] : P[k] ; }
+         )>
+      ) = (
+         (props ) => {
+            return (
+               <WithCurrentACtx>
+               { (aCtx) => (
+               <CACtxtualDestNodeRefUser>
+               { ({ dest, } ) => (
+                  <F10 
+                  {...props }
+                  c={dest }
+                  aCtx={aCtx }
+                  />
+               ) }
+               </CACtxtualDestNodeRefUser>
+               ) }
+               </WithCurrentACtx>
+            ) ;
+         }
+      ) ;
+      F1.displayName = displayName ;
+      return F1 ;
+   }
+) ;
+
+
+const numericOrRElement = (
+   (...[valueArgument0,] : [
+      number | ReactElement ,
+   ] ) => {
+      const valueArgument1 : ReactElement = (
+         typeof valueArgument0 === "number" ?
+         <CConstantValueSrc value={valueArgument0 } />
+         : valueArgument0
+      ) ;
+      const vDisplay = (
+         <code>
+         { (typeof valueArgument0 === "number" ) ? valueArgument0 : `(dynamic)` }
+         </code>
+      ) ;
+      ;
+      return {
+         valueArgument1 ,
+         vDisplay ,
+      } ;
+   }
+) ;
+const CAmpComp = (
+   wrp1("CAmpCompC", CAmpCompImpl, )
+) ;
+const CConstantValueSrc = (
+   wrp1("CConstantValueSrcC", CConstantValueSrcImpl, )
+) ;
+
+

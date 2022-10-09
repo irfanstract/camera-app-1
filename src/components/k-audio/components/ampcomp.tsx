6c42159fc118029d03f240a27f9623e0e6109c59
meh <meh@meh>
2022-10-10 01:27:10 +0700

    init

---
+++ src/components/k-audio/components/ampcomp.tsx
@@ -0,0 +1,66 @@
+
+
+
+
+/**  
+ * obscure the built-in mutable Collections, by design
+ * 
+ */
+ import { 
+   // Seq and List
+   Seq, 
+   List, 
+   Range, 
+   // Set
+   Set, 
+   SortedSet ,
+   // Map
+   Map, 
+   SortedMap ,
+} from "components/util-immutable-datastructure" ;
+import { identity , } from "lodash";
+
+import React, { 
+   // Callbacks
+   Dispatch,
+   DispatchWithoutAction, 
+
+   // Debug
+   useDebugValue,
+   // UseYyyEffect(s)
+   useLayoutEffect, useEffect, 
+   useInsertionEffect ,
+   // UseImperativeHandle
+   useImperativeHandle ,
+   // UseMemo and UseReducer and UseRefObject
+   useCallback, useMemo, useContext, useDeferredValue, 
+   useState, useReducer, 
+   useRef, 
+
+   // Components  
+   Fragment ,
+   ReactElement ,
+   ReactNode ,
+
+} from "react";      
+// import { usePromiseValue1, usePromiseValue, } from './AsyncData';
+
+type ComponentProps<A extends {} & Function > = (
+   (A ) extends { (p: infer P ): unknown ; } ?
+   P : never
+) ;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+

b9075a71f37338e7033bb76b199a33e8a4244e59
meh <meh@meh>
2022-10-10 13:21:02 +0700

    init (2 of 2)

--- src/components/k-audio/components/constantparamsourcenode.tsx
+++ src/components/k-audio/components/constantparamsourcenode.tsx
@@ -74,3 +74,64 @@ type ComponentProps<A extends {} & Function > = (
 
 
 
+function useConstantSrcNde(aCtx : BaseAudioContext) {
+   return (
+      useMemo(() => {
+         const nd = (
+            aCtx
+            .createConstantSource()
+         ) ;
+         nd.start() ;
+         nd.offset.value = 0 ;
+         return nd ;
+      } , [aCtx, ] , )
+   ) ;
+} ;
+
+
+const CConstantValueSrcImpl : (
+   React.FC<(
+      { value : number ; }
+      &
+      { c: AudioNode | AudioParam ; aCtx : BaseAudioContext ; }
+   )>
+) = (
+   ({ c: dest, aCtx, value, }) => {
+      const g1 = (
+         useConstantSrcNde(aCtx, )
+      ) ;
+      useConnectDisconnect(g1, dest, ) ;
+      React["useInsertionEffect"](() => {
+         (
+            g1.offset
+            .setTargetAtTime(value, 0, 0.125, )
+         );
+      } , [value, ] , );
+      // TODO
+      return (
+         <div>
+            <p>
+               constant-source-node : {}
+               <code>{ value }</code> {}
+            </p>
+         </div>
+      ) ;
+   }
+) ;
+
+
+const CConstantValueSrc = (
+   currentAdestnoderefWrpcomp("CConstantValueSrcC", CConstantValueSrcImpl, )
+) ;
+
+
+
+
+
+export {
+   CConstantValueSrc ,
+} ;
+
+
+
+

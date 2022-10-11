e215fdae2e332aac2fdab4fdb4ae0271ccae496f
meh <meh@meh>
2022-10-11 23:41:26 +0700

    extracting (step 2.3 of 2.3 )

--- src/components/k-audio/ctx.tsx
+++ src/components/k-audio/ctx.tsx
@@ -49,6 +49,9 @@ import { useConnectDisconnect, } from "./uacd";
 import {
    CtxValue ,
    ctx ,
+   useInitially1 ,
+   useIWithGivenDestNd1 ,
+   useCtxInferredValues ,
 } from "./ctx-1a" ;
 
 
@@ -69,116 +72,6 @@ export {
    PdMode ,
    TAndTScale ,
 } ;
-const useInitially1 : (
-   (...args : [aCtx : BaseAudioContext, ] ) 
-   => CtxValue
-) = (
-   (...[aCtx, ] ) => {
-      return (
-         useMemo((): CtxValue => ({
-            pd: new PdMode.Stochastically(aCtx.destination, ) ,
-            aCtx: aCtx ,
-            tCtx: TAndTScale.initially() ,
-         }) , [aCtx, ], )
-      ) ;
-   }
-) ;
-const useIWithGivenDestNd1 : (
-   (...args : [AudioNode | AudioParam, ] ) 
-   => (CtxValue | null )
-) = (
-   (...[dest, ] ) => {
-      const presentlyCtxV = (
-         React.useContext(ctx, )
-      ) ;
-      return (
-         useMemo((): null | CtxValue => {
-            const tCtxNew = (
-               presentlyCtxV ?
-               presentlyCtxV.tCtx
-               : TAndTScale.initially()
-            ) ;
-            if (dest instanceof AudioNode ) {
-               return {
-                  tCtx: tCtxNew ,
-                  aCtx : (
-                     dest.context
-                  ) ,
-                  pd : (
-                     new PdMode.Stochastically(dest, )
-                  ) ,
-               } ;
-            }
-            if (presentlyCtxV ) {
-               return {
-                  tCtx: tCtxNew ,
-                  aCtx: (
-                     presentlyCtxV.aCtx
-                  ) ,
-                  pd : (
-                     new PdMode.Stochastically(dest, )
-                  ) ,
-               } ;  
-            }
-            return null ;
-         } , [presentlyCtxV, dest, ], )
-      ) ;
-   }
-) ;
-/**   
- * all values for the point the call is made at.
- * 
- */
-const useCtxInferredValues = (
-   () => {
-      ;
-      const ctxV = (
-         React.useContext(ctx, )
-      ) ;
-      ;
-      return (
-         useMemo((): (
-            (
-               {  tCtxValue : null | ((typeof ctxV ) & object )["tCtx"] ;  } 
-               &
-               (
-                  {  aCtx : null ; dest : null ; } 
-                  |
-                  {  aCtx : BaseAudioContext ; dest : AudioNode | AudioParam ; } 
-               )
-            )
-         ) => {
-            if (ctxV ) {
-               const { 
-                  pd: pdMode , 
-                  aCtx: aCtx ,
-                  tCtx: tCtxValue, 
-               } = ctxV ;
-               if (pdMode instanceof PdMode.OfToSendToDest ) {
-                  const {
-                     dest
-                  } = pdMode ;
-                  return {
-                     tCtxValue ,
-                     dest ,
-                     aCtx ,
-                  } ;
-               }
-               return { 
-                  tCtxValue , 
-                  dest : null ,
-                  aCtx : null , 
-               } ;
-            } 
-            return { 
-               tCtxValue : null , 
-               dest : null ,
-               aCtx : null , 
-            } ;
-         } , [ctxV, ], )
-      ) ;
-   }
-) ;
 type ANFC<A extends AudioNode > = (
    { 
       init : (

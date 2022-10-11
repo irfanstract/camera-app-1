29929a2dc243435ad7a4026d4e264c258bba8491
meh <meh@meh>
2022-10-12 01:27:19 +0700

    init

---
+++ src/components/k-audio/components/plottedsourcenode1.tsx
@@ -0,0 +1,85 @@
+
+
+
+
+/**  
+ * obscure the built-in mutable Collections, by design
+ * 
+ */
+ import { 
+   // Seq and List
+   Seq, 
+   List, 
+   Range, 
+   // Set
+   Set, 
+   SortedSet ,
+   // Map
+   Map, 
+   SortedMap ,
+} from "components/util-immutable-datastructure" ;
+import { identity , } from "lodash";
+
+import React, { 
+   // Callbacks
+   Dispatch,
+   DispatchWithoutAction, 
+
+   // Debug
+   useDebugValue,
+   // UseYyyEffect(s)
+   useLayoutEffect, useEffect, 
+   useInsertionEffect ,
+   // UseImperativeHandle
+   useImperativeHandle ,
+   // UseMemo and UseReducer and UseRefObject
+   useCallback, useMemo, useContext, useDeferredValue, 
+   useState, useReducer, 
+   useRef, 
+
+   // Components  
+   Fragment ,
+   ReactElement ,
+   ReactNode ,
+
+} from "react";      
+// import { usePromiseValue1, usePromiseValue, } from './AsyncData';
+import useMemoisedResource from "components/useMemoisedResource";
+
+import { useConnectDisconnect, } from "components/k-audio/uacd";
+import useConstantSrcNde from "../useConstantSrcNde";
+import { 
+   CToGivenAudioCtxDest, 
+   CWithGivenAFltImpl ,
+   CACtxtualDestNodeRefUser, 
+   WithGivenDestNd ,
+   WithCurrentACtx ,
+} from "components/k-audio/ctx";
+import currentAdestnoderefWrpcomp from "./current-adestnoderef-wrpcomp" ;
+
+type ComponentProps<A extends {} & Function > = (
+   (A ) extends { (p: infer P ): unknown ; } ?
+   P : never
+) ;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {} ;
+

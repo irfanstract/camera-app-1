501fc58aaeb82cd4f7188da03193c46cac7c427f
meh <meh@meh>
2022-10-12 14:43:19 +0700

    add this utility

--- src/components/k-audio/components/loopingcomponent.tsx
+++ src/components/k-audio/components/loopingcomponent.tsx
@@ -125,6 +125,89 @@ type ExportedComponents = {
    ) ;
 
 } ;
+const CFLFT_PROPS_PARSE = (
+   (cProps : ExportedComponentsProps["CFiniteLoopingFromT"] , ) => {
+      ;
+      const { 
+         // START T
+         startT = 0 ,
+         // PERIODS
+         period , 
+      } = cProps ;
+      ;
+      const {
+         endT ,
+         n ,
+      } = (
+         identity<(
+            (props : FiniteLoopingFromT.EndPointProps, ) 
+            => 
+            Required<(
+               Record<keyof FiniteLoopingFromT.EndPointProps, number>
+            ) >
+         )>((cProps  ) => {
+            if (typeof cProps.endT === "number" ) {
+               const {
+                  endT ,
+                  totalDuration = (
+                     endT - startT
+                  ) ,
+                  n = (
+                     totalDuration
+                     / period
+                  ) ,
+               } = cProps ;
+               return {
+                  endT ,
+                  n ,
+                  totalDuration ,
+               } ;
+            }
+            if (typeof cProps.n === "number" ) {
+               const {
+                  n , 
+                  totalDuration = (
+                     n * period
+                  ) ,
+                  endT = (
+                     startT + totalDuration
+                  ) ,
+               } = cProps ;
+               return {
+                  endT ,
+                  n ,
+                  totalDuration ,
+               } ;
+            }
+            if (typeof cProps.totalDuration === "number" ) { 
+               const {
+                  totalDuration ,
+                  n = (
+                     totalDuration 
+                     / period
+                  ) , 
+                  endT = (
+                     startT + totalDuration
+                  ) ,
+               } = cProps ;
+               return {
+                  endT ,
+                  n ,
+                  totalDuration ,
+               } ;
+            }
+            throw TypeError() ;
+         })
+      )(cProps, ) ;
+      ;
+      return {
+         startT ,
+         period ,
+         endT ,
+         n ,
+      } ;
+   }
+) ;
 export const {
    CFiniteLoopingFromT ,
    

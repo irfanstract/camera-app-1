a135ce61bb3261647f66b53c592b31ef8b67cdb8
meh <meh@meh>
2022-10-12 13:04:44 +0700

    init (step 1 of 2)

---
+++ src/components/k-audio/components/loopingcomponent.tsx
@@ -0,0 +1,66 @@
+
+
+
+
+/**  
+ * obscure the built-in mutable Collections, by design
+ * 
+ */
+import { 
+   // Seq and List
+   Seq, 
+   List, 
+   Range, 
+   // Set
+   Set, 
+   SortedSet ,
+   // Map
+   Map, 
+   SortedMap ,
+} from "components/util-immutable-datastructure" ;
+import { identity , } from "lodash";
+import { clamp, } from "lodash";
+
+import React, { 
+   // Callbacks
+   Dispatch,
+   DispatchWithoutAction, 
+
+   // Debug
+   useDebugValue,
+   // UseYyyEffect(s)
+   useLayoutEffect, useEffect, 
+   useInsertionEffect ,
+   // UseImperativeHandle
+   useImperativeHandle ,
+   // UseMemo and UseReducer and UseRefObject
+   useCallback, useMemo, useContext, useDeferredValue, 
+   useState, useReducer, 
+   useRef, 
+
+   // Components  
+   Fragment ,
+
+} from "react";      
+import { ComponentProps, } from "components/util-jsx-props";
+// import { EnumValueDisplayElem, } from "components/json-display/enum-value-display";
+import useMemoisedResource from "components/useMemoisedResource";
+// import { usePromiseValue1, usePromiseValue, } from './AsyncData';
+
+import { CToGivenAudioCtxDest, CACtxtualDestNodeRefUser, } from "components/k-audio/ctx";
+import { WithGivenDestNd, } from "components/k-audio/ctx";
+import {  
+   CMereProductiveElem ,
+   CMereMonitoringElem ,
+} from "components/k-audio/ctx";
+import currentAdestnoderefWrpcomp from "./current-adestnoderef-wrpcomp" ;
+
+
+
+
+
+
+
+
+
+export {} ;

9b3318a1db110a3ac6fd6a30c02f17cf036ff4df
meh <meh@meh>
2022-10-12 08:29:18 +0700

    init (step 1 of 2)

---
+++ src/components/k-audio/ctx-1a0.tsx
@@ -0,0 +1,67 @@
+
+
+
+// m1-KAudio-ctx-1a-1
+
+/**  
+ * obscure the built-in mutable Collections, by design
+ * 
+ */
+ import { 
+   // Seq and List
+   Seq, 
+   List, 
+   Range, 
+   // Set
+   Set, 
+   SortedSet ,
+   // Map
+   Map, 
+   SortedMap ,
+} from "components/util-immutable-datastructure" ;
+import { identity , } from "lodash";
+import { TAndTScale, } from "./TOffsetAndScaleProperties";
+import { PdMode, } from "./graph-modes";
+
+import React, { 
+   // Callbacks
+   Dispatch,
+   DispatchWithoutAction, 
+
+   // Debug
+   useDebugValue,
+   // UseYyyEffect(s)
+   useLayoutEffect, useEffect, 
+   useInsertionEffect ,
+   // UseImperativeHandle
+   useImperativeHandle ,
+   // UseMemo and UseReducer and UseRefObject
+   useCallback, useMemo, useContext, useDeferredValue, 
+   useState, useReducer, 
+   useRef, 
+
+   // Components  
+   Fragment ,
+
+} from "react";      
+// import { usePromiseValue1, usePromiseValue, } from './AsyncData';
+
+import { useConnectDisconnect, } from "./uacd";
+
+
+
+
+
+
+
+
+
+
+
+
+
+;  
+
+
+
+

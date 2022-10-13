47b278198f53cc4db22c37026d62bbb09bfd76ec
meh <meh@meh>
2022-10-13 23:58:47 +0700

    `CCPS_IMPL.apTAndTScaleTranslatedForm` `exponentialRampToValueAtTime` (impl )

--- src/components/k-audio/components/plottedsourcenode1.tsx
+++ src/components/k-audio/components/plottedsourcenode1.tsx
@@ -244,6 +244,15 @@ namespace CCPS_IMPL {
                   }
                   return this as AudioParam ;
                },
+               exponentialRampToValueAtTime(targetedValue, specifiedEndT, ) {
+                  if ((
+                     true
+                     && STVT("exponentialRampToValueAtTime" , { value: targetedValue, t: specifiedEndT, } , )
+                  )) {
+                  } else {
+                  }
+                  return this as AudioParam ;
+               },
                cancelScheduledValues(specifiedT, ) {
                   if ((
                      true

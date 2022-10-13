df999567a721c02129b966b8d905c1961a289701
meh <meh@meh>
2022-10-13 23:07:28 +0700

    extracting `CCPS_IMPL.apTAndTScaleTranslatedForm` (step 3 of 3)

--- src/components/k-audio/components/plottedsourcenode1.tsx
+++ src/components/k-audio/components/plottedsourcenode1.tsx
@@ -179,30 +179,11 @@ const CCPS_IMPL_1A : (
       const [ , refresh, ] = (
       useReducer(() => {
          g1.offset.cancelScheduledValues(0, ) ;
-         cb(...((p: AudioParam , ) : Parameters<typeof cb > => {
-            const {
-               minValue ,
-               maxValue ,
-            } = p ;
-            return [
-               {
-                  minValue ,
-                  maxValue ,
-                  get value() {
-                     return p.value ;
-                  } ,
-                  set value(newVal: number , ) {
-                     (
-                        p.value = newVal
-                     ) ;
-                  } ,
-                  // TODO
-                  setTargetAtTime() {
-                     return this as AudioParam ;
-                  } ,
-               } ,
-            ] ;
-         } )(g1.offset , ) ) ;
+         cb((
+            CCPS_IMPL.apTAndTScaleTranslatedForm((
+               g1.offset
+            ), tCtxVal, )
+         ) ) ;
       } , void 0 , )
       ) ;
       React["useLayoutEffect"](() => (

c9a35e459058adbc3adef48bc1b0b1b2fe67b8bf
meh <meh@meh>
2022-10-15 23:46:37 +0700

    amend

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import Demo001 from 'components/k-audio/demo-001';
+import DemoBassdrums001 from 'components/k-audio/demo-bassdrums-001';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -17,7 +17,7 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <Demo001 />
+        <DemoBassdrums001 />
         <ExploreContainer name="Tab 2 page" />
       </IonContent>
     </IonPage>

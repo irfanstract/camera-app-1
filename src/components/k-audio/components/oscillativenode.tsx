df5c2ea0072aa8fcdde1630f457e4d26c8676e10
meh <meh@meh>
2022-10-16 16:55:35 +0700

    add `WFreqAndDetuneProperties`

--- src/components/k-audio/components/oscillativenode.tsx
+++ src/components/k-audio/components/oscillativenode.tsx
@@ -160,6 +160,12 @@ const waveTableAssignPw : (
 
 
 
+type WFreqAndDetuneProperties = (
+   {}
+   &
+   { [k in keyof { f?: true ; /** cents-of-semitones to detune */ det?: true ; } ] : number | React.ReactElement ; }
+) ;
+
 const CWaveTableImpl : (
    React.FC<(
       { type : KWaveShape ; }

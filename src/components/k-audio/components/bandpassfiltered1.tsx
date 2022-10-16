e5f44429860b45051d323316f8cd7b05c7a01e0f
meh <meh@meh>
2022-10-17 00:46:38 +0700

    init

---
+++ src/components/k-audio/components/bandpassfiltered1.tsx
@@ -0,0 +1,92 @@
+
+
+
+
+/**  
+ * obscure the built-in mutable Collections, by design
+ * 
+ */
+ import { 
+   // Seq and List
+   Seq, 
+   List, 
+   Range, 
+   // Set
+   Set, 
+   SortedSet ,
+   // Map
+   Map, 
+   SortedMap ,
+} from "components/util-immutable-datastructure" ;
+import { identity , } from "lodash";
+import { clamp, } from "lodash";
+
+import React, { 
+   // Callbacks
+   Dispatch,
+   DispatchWithoutAction, 
+
+   // Debug
+   useDebugValue,
+   // UseYyyEffect(s)
+   useLayoutEffect, useEffect, 
+   useInsertionEffect ,
+   // UseImperativeHandle
+   useImperativeHandle ,
+   // UseMemo and UseReducer and UseRefObject
+   useCallback, useMemo, useContext, useDeferredValue, 
+   useState, useReducer, 
+   useRef, 
+
+   // Components  
+   Fragment ,
+   ReactElement ,
+   ReactNode ,
+
+} from "react";      
+import { ComponentProps, } from "components/util-jsx-props";
+import useMemoisedResource from "components/useMemoisedResource";
+import { 
+   EnumValueDisplayElem, 
+} from "components/json-display/enum-value-display";
+
+import { WithGivenDestNd, } from "../ctx";
+import { 
+   numericOrRElement ,
+   CConstantValueSrc ,
+} from "./constantparamsourcenode";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {} ;
+
+
+
+
+

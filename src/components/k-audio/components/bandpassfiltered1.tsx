22a7cc345752602d33f90ecb4fbd3e0a50b0ab9c
meh <meh@meh>
2022-10-17 02:36:31 +0700

    add `CBandPassFiltered` (def)

--- src/components/k-audio/components/bandpassfiltered1.tsx
+++ src/components/k-audio/components/bandpassfiltered1.tsx
@@ -85,6 +85,148 @@ import { FreqAndDetuneProperties, } from "./freqAndDetuneProperties";
 
 
 
+type CBPF_PICK<A, B extends A > = (
+   B
+) ;
+const CBandPassFiltered = (
+   identity<(
+      React.FC<(
+         {}
+         & Required<React.PropsWithChildren >
+         & (
+            never
+            | {
+               [fltType in BiquadFilterType ] : (
+                  {}
+                  & { type : fltType ; }
+                  & (
+                     fltType extends (
+                        never 
+                        | CBPF_PICK<BiquadFilterType, "lowpass" >
+                        | CBPF_PICK<BiquadFilterType, "highpass" >
+                        | CBPF_PICK<BiquadFilterType, "bandpass" >
+                        | CBPF_PICK<BiquadFilterType, "notch" >
+                     ) ? 
+                     { gain ?: never ; }
+                     : { gain : Required<FreqAndDetuneProperties>["f"] ; }
+                  )
+                  & (
+                     {}
+                     & Required<Pick<FreqAndDetuneProperties , "f" > >
+                     & Partial <Pick<FreqAndDetuneProperties , "det" > >
+                  )
+                  & { Q ?: Required<FreqAndDetuneProperties>["f"] ; }
+               ) ;
+            }[BiquadFilterType ]
+         )
+      )>
+   )>(function CBandPassFiltered(props , ) {
+      const { children: payload, } = props ;
+      const { type: specifiedType, } = props ;
+      const { 
+         f = 440 , // best-effort
+         det: specifiedDetune = 0 ,
+         gain: specifiedAmpVal = 1 ,
+         Q: specifiedQValue0 ,
+      } = props ;
+      const {
+         aCtx ,
+         dest ,
+      } = useCtxInferredValues() ;
+      const [nd1, ] = (
+         useMemoisedResource<[null | BiquadFilterNode, ]>((oldV, ) => {
+            if (oldV ) {
+               const [oldNd, ] = oldV ;
+               ;
+            }
+            {
+               const newNd = (
+                  aCtx
+                  && aCtx.createBiquadFilter()
+               ) ;
+               newNd && (
+                  [
+                     newNd.frequency, 
+                     newNd.detune, 
+                     newNd.gain, 
+                     newNd.Q, 
+                  ]
+                  .forEach((aP: AudioParam, ) => {
+                     aP.setValueAtTime((
+                        clamp(0, aP.minValue, aP.maxValue, )
+                     ) , 0 , ) ;
+                  } )
+               ) ;
+               return [
+                  newNd ,
+               ] ;
+            }
+         } , [aCtx, ] , )
+      ) ;
+      useConnectDisconnect(nd1, dest, ) ;
+      React["useInsertionEffect"](() => {
+         if (nd1 ) {
+            nd1.type = specifiedType ;
+         }
+      } , [nd1, specifiedType, ] ) ;
+      return (
+         nd1
+         ?
+         <div>
+            <p>
+               Biquad-Filtered
+            </p>
+            <table>
+            <tbody>
+            <tr>
+               <td>Type</td>
+               <td>
+                  <code>{ specifiedType }</code>
+               </td>
+            </tr>
+            <tr>
+               <td>Frequency</td>
+               <td>
+                  Frequency :
+                  <WithGivenDestNd 
+                  value={nd1.frequency } 
+                  children={numericOrRElement(f, ).valueArgument1 }
+                  />
+                  ;
+                  Detune :
+                  <WithGivenDestNd 
+                  value={nd1.detune } 
+                  children={numericOrRElement(specifiedDetune, ).valueArgument1 }
+                  />
+                  ;
+               </td>
+            </tr>
+            <tr>
+               <td>Gain</td>
+               <td>
+                  <WithGivenDestNd 
+                  value={nd1.gain } 
+                  children={numericOrRElement(specifiedAmpVal, ).valueArgument1 }
+                  />
+               </td>
+            </tr>
+            <tr>
+               <td>PAYLOAD</td>
+               <td>
+                  <WithGivenDestNd 
+                  value={nd1 } 
+                  children={payload }
+                  />
+               </td>
+            </tr>
+            </tbody>
+            </table>
+         </div>
+         :
+         null
+      ) ;
+   } )
+) ;
 
 
 

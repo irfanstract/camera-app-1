ccb4927598e1a411625b6693048f4e1900f37042
meh <meh@meh>
2022-10-17 13:14:09 +0700

    `CPlottedSourceNode1C` `useXADestParamEffect` add `dependencies` (impl-fetch)

--- src/components/k-audio/components/plottedsourcenode1.tsx
+++ src/components/k-audio/components/plottedsourcenode1.tsx
@@ -302,6 +302,7 @@ const CPlottedSourceNode1C = (() => {
    ) = (
       (callback, options, ) => {
          const { // OPTIONS
+            dependencies ,
          } = options || {} ;
          const {
             tCtxValue : tCtxVal ,

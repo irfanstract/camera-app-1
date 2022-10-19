af24451df6c0666411200b8669d8adeee8aa81e1
meh <meh@meh>
2022-10-19 19:18:58 +0700

    ````` import React, {   // Callbacks   Dispatch,   DispatchWithoutAction,
      // Debug
      useDebugValue,
      // UseYyyEffect(s)
      useLayoutEffect, useEffect,
      useInsertionEffect ,
      // UseImperativeHandle
      useImperativeHandle ,
      // UseMemo and UseReducer and UseRefObject
      useCallback, useMemo, useContext, useDeferredValue,
      useState, useReducer,
      useRef,
    
      // Components
      Fragment ,
      ReactElement ,
      ReactNode ,
    
    } from 'react';
    ````

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,28 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import React, { 
+  // Callbacks
+  Dispatch,
+  DispatchWithoutAction, 
+
+  // Debug
+  useDebugValue,
+  // UseYyyEffect(s)
+  useLayoutEffect, useEffect, 
+  useInsertionEffect ,
+  // UseImperativeHandle
+  useImperativeHandle ,
+  // UseMemo and UseReducer and UseRefObject
+  useCallback, useMemo, useContext, useDeferredValue, 
+  useState, useReducer, 
+  useRef, 
+
+  // Components  
+  Fragment ,
+  ReactElement ,
+  ReactNode ,
+
+} from 'react';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {

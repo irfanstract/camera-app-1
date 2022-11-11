9021699011d4813872fc4055a6dae9f12648adce
meh <meh@meh>
2022-11-11 10:40:18 +0700

    `src\pages\Tab3.tsx`

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,25 +1,17 @@
+import React, {} from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import AvGenApp1 from 'components/ffmpg-renderdemo-app1';
+import AvConvApp from 'pages/AvConvApp';
 
 const Tab3: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
-    </IonPage>
+    <AvConvApp />
   );
 };
+{}
+{}
+{}
 
 export default Tab3;
